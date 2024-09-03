43db31c1953f46f2815b220372fac0088730f0a6
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-09-03 16:51:18 +0200

    feat: refactor with components now in personal files

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,6 @@
-import img from "./assets/react-core-concepts.png";
-import componentsImg from "./assets/components.png"
+import Header from "./components/Header.jsx";
+import CoreConcept from "./components/CoreConcepts.jsx";
 import { CORE_CONCEPTS } from "./data.js";
-
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-// random index generator
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max+1))
-};
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(reactDescriptions.length)];
-
-  return (
-    <header>
-      <img src={img} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        { description }  React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-// function CoreConcept(props) {
-//   return( 
-//   <li>
-//     <img src={props.image} alt={props.title} />
-//     <h3>{props.title}</h3>
-//     <p>{props.description}</p>
-//   </li>
-//   )
-// }
-
-// same code destructured
-  function CoreConcept({title, image, description}) {
-    return( 
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-    )
-  }
-
 
 function App() {
   return (
@@ -61,7 +18,6 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
-
         </ul>
       </section>
       <main>
