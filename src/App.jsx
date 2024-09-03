f8be47af35447da469949343fd3688d7b80e77d1
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-09-03 16:34:02 +0200

    feat: props with destructuring

--- src/App.jsx
+++ src/App.jsx
@@ -23,15 +23,27 @@ function Header() {
   );
 }
 
-function CoreConcept(props) {
-  return( 
-  <li>
-    <img src={props.image} alt="" />
-    <h3>{props.title}</h3>
-    <p>{props.description}</p>
-  </li>
-  )
-}
+// function CoreConcept(props) {
+//   return( 
+//   <li>
+//     <img src={props.image} alt={props.title} />
+//     <h3>{props.title}</h3>
+//     <p>{props.description}</p>
+//   </li>
+//   )
+// }
+
+// same code destructured
+  function CoreConcept({title, image, description}) {
+    return( 
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+    )
+  }
+
 
 function App() {
   return (
