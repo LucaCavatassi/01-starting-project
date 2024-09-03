a7285f493b8ebad33565159b9a6da0bcf12c14c5
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-09-03 17:01:05 +0200

    feat: subfolders created

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import "./Header.css";
-import img from "../assets/react-core-concepts.png";
+import img from "../../assets/react-core-concepts.png";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 // random index generator
