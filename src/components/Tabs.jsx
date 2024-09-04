196a73c18a7fea0338388ab7b5b68c3ad36ebbb1
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-09-04 18:06:49 +0200

    feat: button container props

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,9 +1,11 @@
-export default function Tabs({ children, buttons}) {
+export default function Tabs({ children, buttons, buttonsContainer}) {
+    const ButtonContainer = buttonsContainer;
+
     return (
         <>
-            <menu>
+            <ButtonContainer>
                 {buttons}
-            </menu>
+            </ButtonContainer>
             {children}
         </>
     )
