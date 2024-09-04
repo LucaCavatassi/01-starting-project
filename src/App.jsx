5ef41ffe80ba150d69f477504c6fd228f56eb7c2
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-09-04 11:47:06 +0200

    feat: cleaner code

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,21 @@ function App() {
   
   function handleClick(selectedButton) {
         setSelectedTopic(selectedButton)
-        console.log(selectedTopic);
-        
       }
+  
+  let tabContent = <p> Please select a topic. </p>
 
+  if (selectedTopic) {
+    tabContent = <div id="tab-content">
+                  <h3> {EXAMPLES[selectedTopic].title} </h3>
+                  <p>{EXAMPLES[selectedTopic].description} </p>
+                  <pre>
+                    <code>
+                      {EXAMPLES[selectedTopic].code} 
+                    </code>
+                  </pre>
+                </div> 
+  }
 
   return (
     <div>
@@ -49,18 +60,7 @@ function App() {
             <TabButton click={() => handleClick("state")} > State </TabButton>
           </menu>
           {/* Dynamic Content */}
-            {!selectedTopic ? <p> Please Select a topic. </p> : 
-            <div id="tab-content">
-              <h3> {EXAMPLES[selectedTopic].title} </h3>
-              <p>{EXAMPLES[selectedTopic].description} </p>
-              <pre>
-                <code>
-                  {EXAMPLES[selectedTopic].code} 
-                </code>
-              </pre>
-            </div> 
-            }
-            
+          {tabContent}
         </section>
       </main>
     </div>
